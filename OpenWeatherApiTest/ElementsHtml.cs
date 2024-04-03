fc240950d74bb7557201561100e2eadcd52224cf
kottok2205 <tarasyuk222001@gmail.com>
2024-04-03 15:57:43 +0300

    Обновить 58441923-de3f-4c9f-b798-539a94711bc1.vsidx и .futdcache.v2 и ещё 6 файла...

--- OpenWeatherApiTest/ElementsHtml.cs
+++ OpenWeatherApiTest/ElementsHtml.cs
@@ -14,7 +14,6 @@ namespace OpenWeatherApiTest
 
             string[] dateArray = new string[8];
 
-            // Очікуємо, доки не буде знайдений хоча б один елемент відповідно до виразу XPath
             for (int i = 0, j = 1; i < 8; i++, j++)
             {
                 var dayElement = doc.DocumentNode.SelectNodes(@$"//*[@id=""weather-widget""]/div[2]/div[2]/div[2]/ul/li[{j}]/span");
