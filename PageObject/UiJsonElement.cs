a66242eddf31b40621eb067d0e5d73c21a301b10
kottok2205 <tarasyuk222001@gmail.com>
2024-04-24 17:23:36 +0300

    Обновить .dtbcache.v2 и 1f4bc7eb-8185-4a50-a5da-84a7a1f1b6a9.vsidx и ещё 16 файла...

--- PageObject/UiJsonElement.cs
+++ PageObject/UiJsonElement.cs
@@ -1,37 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V121.Accessibility;
 
 namespace PageObject
 {
-    public class UiJsonElement
+    public class UiJsonElement : BasePage
     {
-        public string[] dateArrayJson(string jsonFile)
-        {
-            CultureInfo culture = new CultureInfo("en-US");
+        //public static void SelDriver()
+        //{
+        //    var service = ChromeDriverService.CreateDefaultService();
+        //    service.LogPath = AppDomain.CurrentDomain.BaseDirectory + "chromedriver.log";
+        //    service.EnableVerboseLogging = true;
 
-            JsonDocument jsonDoc = JsonDocument.Parse(jsonFile);
+        //    var options = new ChromeOptions();
 
-            var root = jsonDoc.RootElement;
-            var dailyArray = root.GetProperty("daily");
-            JsonElement[] dtArray = dailyArray.EnumerateArray().Select(x => x.GetProperty("dt")).ToArray();
+        //    var webDriver = new ChromeDriver(service, options);
 
-            string[] dateArray = new string[8];
+        //    var devToolsSession = webDriver.GetDevToolsSession();
+        //    devToolsSession.GetVersionSpecificDomains<NetworkDomain>(out var network).Enable(new EnableCommandSettings());
+        //}
 
-            for (int i = dtArray.Length - 8, j = 0; i < dtArray.Length; i++, j++)
-            {
-                long unixTime = dtArray[i].GetInt64();
 
-                var dateTime = DateTimeOffset.FromUnixTimeSeconds(unixTime).UtcDateTime;
 
-                var formattedDate = dateTime.ToString("ddd, MMM dd", culture);
-                dateArray[j] = formattedDate;
-            }
-            return dateArray;
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //public string[] dateArrayJson()
+        //{
+        //    Driver.Navigate().GoToUrl("https://openweathermap.org/data/2.5/onecall?lat=46.4775&lon=30.7326&units=metric&appid=439d4b804bc8187953eb36d2a8c26a02");
+
+        //    string responseData = Driver.FindElement(By.TagName("body")).Text;
+
+        //    var normalizedExpected = Regex.Replace(responseData, @"\s+", "");
+
+        //    CultureInfo culture = new CultureInfo("en-US");
+
+        //    JsonDocument jsonDoc = JsonDocument.Parse(normalizedExpected);
+
+        //    var root = jsonDoc.RootElement;
+        //    var dailyArray = root.GetProperty("daily");
+        //    JsonElement[] dtArray = dailyArray.EnumerateArray().Select(x => x.GetProperty("dt")).ToArray();
+
+        //    string[] dateArray = new string[8];
+
+        //    for (int i = dtArray.Length - 8, j = 0; i < dtArray.Length; i++, j++)
+        //    {
+        //        long unixTime = dtArray[i].GetInt64();
+
+        //        var dateTime = DateTimeOffset.FromUnixTimeSeconds(unixTime).UtcDateTime;
+
+        //        var formattedDate = dateTime.ToString("ddd, MMM dd", culture);
+        //        dateArray[j] = formattedDate;
+        //    }
+        //    return dateArray;
+        //}
     }
 }
